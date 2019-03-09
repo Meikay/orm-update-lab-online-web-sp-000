8f0e38759a64886b61da709fbd7b59102d9fde72
Meisuleen Kased <meisuleen@gmail.com>
2019-03-09 18:41:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     sql = "SELECT * FROM songs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
-    end
+
   end
 
 end
