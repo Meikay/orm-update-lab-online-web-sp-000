87ef91135152c1748a49de03cd62a5fa8e637443
Meisuleen Kased <meisuleen@gmail.com>
2019-03-09 18:41:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,4 +53,11 @@ class Student
     self.new(id, name, grade)
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+    end
+  end
+
 end
