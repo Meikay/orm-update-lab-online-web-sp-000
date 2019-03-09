4c71c489e217d2ee030b08e8dfc40670b81f8cc4
Meisuleen Kased <meisuleen@gmail.com>
2019-03-09 18:27:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ class Student
     students
   end
 
-  def self.new_from_db(row)
+  def self.new_from_db(id, name, grade)
     student = self.new
     student.id = row[0]
     student.name = row[1]
